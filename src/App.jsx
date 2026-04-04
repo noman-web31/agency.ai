e2408e8b9e7e4be76479b506066b600a178ace36
Noman1331 <nomananoosh.0091@gmail.com>
2026-04-05 00:08:41 +0500

    hero section has been made

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
-import './App.css'
+import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 const App = () => {
+
+  const [theme, setTheme] = useState('light')
+
   return (
     <>
- <Navbar />
+      <div className='dark:bg-black relative'>
+        <Navbar theme={theme} setTheme={setTheme} />
+        <Hero />
+      </div>
     </>
   )
 }
