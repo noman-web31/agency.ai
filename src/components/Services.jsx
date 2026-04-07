2be2ff706df9a6d4df6aab84839064766d362bad
Noman1331 <nomananoosh.0091@gmail.com>
2026-04-07 10:50:17 +0500

    service section made

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import assets from '../assets/assets'
 import Title from './Title'
+import ServiceCard from './ServiceCard'
 
 const Services = () => {
   
@@ -11,12 +12,17 @@ const Services = () => {
            icon: assets.ads_icon 
         },
         {
+           title: "Content Marketing",
+           description: "We help you execute your plan and deliver results.",
+           icon: assets.marketing_icon 
+        },
+        {
            title: "Content Writing",
            description: "We help you create a marketing atrategy that drives results.",
            icon: assets.content_icon
         },
         {
-           title: "Social media ",
+           title: "Social media",
            description: "We help you build a strong social media presence and engage with your audience.",
            icon: assets.social_icon
         }
@@ -25,9 +31,18 @@ const Services = () => {
     return (
     <div id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 '>
 
-<img src={assets.bgImage2} alt="" className='absolute -top-110 -left-70 -z-1'/>
+      <img src={assets.bgImage2} alt="" className='absolute -top-110 -left-70 -z-10'/>
 
-<Title title='How can we help?' desc='From strategy to execution, we craft digital soution that move your business forward.'/>
+      <Title 
+        title='How can we help?' 
+        desc='From strategy to execution, we craft digital solution that move your business forward.'
+      />
+
+      <div className='flex flex-col md:grid grid-cols-2'>
+        {servicesData.map((service, index) => (
+          <ServiceCard key={index} service={service} index={index} />
+        ))}
+      </div>
 
     </div>
   )
